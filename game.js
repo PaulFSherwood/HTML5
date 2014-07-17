6619cd1fdf0ff887e472c35c5ee8cde2de8a1f02
PaulFSherwood <paulfsherwood@gmail.com>
2014-07-17 11:47:46 -0600

    update game.js to add inner id box

---
+++ game.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',init,false);
+
+function init() {
+    document.getElementById('inner').innerHTML='<canvas id="canvasBg" width="800px" height="500px" style="display:block;background:#ffffff;margin:100px auto 0px;"></canvas>';
+}
