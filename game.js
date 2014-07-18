2e1e3a335c68a69c815354122c6a74185621624b
PaulFSherwood <paulfsherwood@gmail.com>
2014-07-18 02:23:18 -0600

    updated .html to hold the buttons, updated game.js to draw stuff and erase stuff.  added a raw/png pic and associated folder

--- game.js
+++ game.js
@@ -1,5 +1,18 @@
-window.addEventListener('load',init,false);
+var canvasBg = document.getElementById('canvasBg');
+var ctxBg = canvasBg.getContext('2d');
 
-function init() {
-    document.getElementById('inner').innerHTML='<canvas id="canvasBg" width="800px" height="500px" style="display:block;background:#ffffff;margin:100px auto 0px;"></canvas>';
+var clearCanvasBtn = document.getElementById('clearCanvasBtn');
+clearCanvasBtn.addEventListener('click', clearCanvas, false);
+
+var drawSquareBtn = document.getElementById('drawSquareBtn');
+drawSquareBtn.addEventListener('click',drawSquare,false);
+
+
+function drawSquare() {
+    ctxBg.fillStyle = '#505050';
+    ctxBg.fillRect(20,100,600,600);
+}
+
+function clearCanvas() {
+    ctxBg.clearRect(0,0,800,500);
 }
