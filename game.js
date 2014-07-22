77db3602247c717522b82865ec6d1d235cd3ea2e
PaulFSherwood <paulfsherwood@gmail.com>
2014-07-22 11:35:11 -0600

    adding css cleanup

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
+// background
 var canvasBg = document.getElementById('canvasBg');
 var ctxBg = canvasBg.getContext('2d');
+// jet
+var canvasJet = document.getElementById('canvasJet');
+var ctxJet = canvasJet.getContext('2d');
+
 
 var clearCanvasBtn = document.getElementById('clearCanvasBtn');
 clearCanvasBtn.addEventListener('click', clearCanvas, false);
@@ -10,7 +15,22 @@ var gameHeight = canvasBg.height;
 // loading images
 var imgSprite = new Image();
 imgSprite.src = 'img/sprite.png';
-imgSprite.addEventListener('load',drawBg, false);
+imgSprite.addEventListener('load',init, false);
+
+function init(){
+    drawJet();
+    drawBg();
+}
+
+function drawJet(){
+    var srcX = 0;
+    var srcY = 601;
+    var drawX = 150;
+    var drawY = 250;
+    var width = 100;
+    var height = 40;
+    ctxJet.drawImage(imgSprite, srcX, srcY, width, height, drawX, drawY, width, height);
+}
 
 function drawBg(){
     var srcX = 0;
@@ -18,7 +38,6 @@ function drawBg(){
     var drawX = 0;
     var drawY = 0;
     ctxBg.drawImage(imgSprite, srcX, srcY, gameWidth, gameHeight, drawX, drawY, gameWidth, gameHeight);
-
 }
 
 function clearCanvas() {
