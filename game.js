8526c8ebcbc4bc229639d152c8fca3db4d71b920
PaulFSherwood <paulfsherwood@gmail.com>
2014-07-23 01:15:35 -0600

    update draw functions

--- game.js
+++ game.js
@@ -5,12 +5,10 @@ var ctxBg = canvasBg.getContext('2d');
 var canvasJet = document.getElementById('canvasJet');
 var ctxJet = canvasJet.getContext('2d');
 
-
-var clearCanvasBtn = document.getElementById('clearCanvasBtn');
-clearCanvasBtn.addEventListener('click', clearCanvas, false);
-
 var gameWidth = canvasBg.width;
 var gameHeight = canvasBg.height;
+var fps = 10;
+var drawInterval;
 
 // loading images
 var imgSprite = new Image();
@@ -18,18 +16,40 @@ imgSprite.src = 'img/sprite.png';
 imgSprite.addEventListener('load',init, false);
 
 function init(){
-    drawJet();
     drawBg();
+    startDrawing();
 }
 
+function draw(){
+    drawJet();
+}
+
+function startDrawing(){
+	stopDrawing();
+    drawInterval = setInterval(draw, fps);
+}
+
+function stopDrawing(){
+	// reset our interval
+	clearInterval(drawInterval);
+}
+
+function Jet(){
+    this.srcX   =   0;
+    this.srcY   = 601;
+    this.drawX  = 150;
+    this.drawY  = 250;
+    this.width  = 100;
+    this.height =  40;
+
+}
+
+Jet.prototype.draw = function() {
+    clearCtxJet();
+    ctxJet.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+};
+
 function drawJet(){
-    var srcX = 0;
-    var srcY = 601;
-    var drawX = 150;
-    var drawY = 250;
-    var width = 100;
-    var height = 40;
-    ctxJet.drawImage(imgSprite, srcX, srcY, width, height, drawX, drawY, width, height);
 }
 
 function drawBg(){
@@ -40,6 +60,10 @@ function drawBg(){
     ctxBg.drawImage(imgSprite, srcX, srcY, gameWidth, gameHeight, drawX, drawY, gameWidth, gameHeight);
 }
 
-function clearCanvas() {
+function clearCtxBg() {
     ctxBg.clearRect(0,0,800,500);
+}
+
+function clearCtxJet() {
+	ctxJet.clearRect(0, 0, gameWidth, gameHeight);
 }
