fe9f1d0ebb23499ed9d64abd9c4bb9c73a66b655
PaulFSherwood <paulfsherwood@gmail.com>
2014-07-26 10:16:00 -0600

    comment updates

--- test003/game.js
+++ test003/game.js
@@ -21,7 +21,7 @@ states = {
 	Splash: 0, Game: 1, Score: 2
 },
 
-// Game objects
+// Game objects //
 
 /**
   * Ok button initiated in main()
@@ -36,39 +36,54 @@ bird = {
     x: 60,
     y: 0,
     frame: 0,
-    animation: [0, 1, 2, 1],
+    velocity: 0,
+    animation: [0, 1, 2, 1], // animation sequence
+
     rotation: 0,
     radius: 12,
+
     gravity: 0.25,
     _jump: 4.6,
 
+	/**
+	 * Makes the bird "flap" and jump
+	 */
     jump: function() {
         this.velocity = -this._jump;  // why is this _jump and not just jump
     },
 
+    /**
+     * Update sprite animation and position of bird
+     */
     update: function() {
+        // make sure animation updates and plays faster in gamestate
         var n = currentstate === states.Splash ? 10 : 5;
         
         this.frame += frames % n === 0 ? 1 : 0; // why do we need to watch the frames
         this.frame %= this.animation.length;
 
-        // floating up and down
+	// in splash state make bird hover up and down and set
+        // rotation to zero
         if (currentstate === states.Splash) {
 
 			this.y = height - 280 + 5*Math.cos(frames/10);
 			this.rotation = -0.1;
-        } else if (currentstate === states.Game) {
+        } else if (currentstate === states.Game) { // game and score state //
             this.velocity += this.gravity;
             this.y += this.velocity;
             
+                        // change to the score state when bird touches the ground
             if (this.y >= height - s_fg.height -10) {
                 this.y = height - s_fg.height - 10;
                 if (currentstate === states.Game) {
                     currentstate = states.Score;
                 }
+		// sets velocity to jump speed for correct rotation
                 this.velocity = this._jump;
             }
 
+	    // when bird lack upward momentum increment the rotation
+	    // angle
             if (this.velocity >= this._jump) {
                 this.frame = 1;
                 this.rotation = Math.min(Math.PI/2, this.rotation + 0.3);
@@ -78,6 +93,12 @@ bird = {
         } 
     },
 
+	/**
+	 * Draws bird with rotation to canvas ctx
+	 * 
+	 * @param  {CanvasRenderingContext2D} ctx the context used for
+	 *                                        drawing
+	 */
 	draw: function(ctx) {
 		ctx.save();
 		// translate and rotate ctx coordinatesystem
@@ -96,18 +117,31 @@ bird = {
         //--ctx.fill();
 
 		ctx.restore();
-    }
+	}
 },
 
+/**
+ * The pipes
+ */
 pipes = {
     
     _pipes: [],
+	// padding: 80, // TODO: Implement paddle variable
 
+	/**
+	 * Empty pipes array
+	 */
     reset: function() {
-    },
+                this._pipes = [];
+	},
 
-    update: function() {
-        if (frames % 100 === 0) {
+	/**
+	 * Create, push and update all pipes in pipe array
+	 */
+	update: function() {
+		// add new pipe each 100 frames
+		if (frames % 100 === 0) {
+			// calculate y position
 			var _y = height - (s_pipeSouth.height+s_fg.height+120+200*Math.random());
 			// create and push pipe to array
 			this._pipes.push({
@@ -116,7 +150,7 @@ pipes = {
 				width: s_pipeSouth.width,
 				height: s_pipeSouth.height
 			});
-        }
+		}
 		for (var i = 0, len = this._pipes.length; i < len; i++) {
 			var p = this._pipes[i];
 
@@ -130,7 +164,6 @@ pipes = {
 				var cx  = Math.min(Math.max(bird.x, p.x), p.x+p.width);
 				var cy1 = Math.min(Math.max(bird.y, p.y), p.y+p.height);
 				var cy2 = Math.min(Math.max(bird.y, p.y+p.height+80), p.y+2*p.height+80);
-
 				// closest difference
 				var dx  = bird.x - cx;
 				var dy1 = bird.y - cy1;
@@ -144,7 +177,6 @@ pipes = {
 					currentstate = states.Score;
 				}
 			}
-            
 			// move pipe and remove if outside of canvas
 			p.x -= 2;
 			if (p.x < -p.width) {
@@ -170,16 +202,23 @@ pipes = {
 	}
 };
 
-// change game state
-function onpress() {
+/**
+ * Called on mouse or touch press. Update and change state
+ * depending on current game state.
+ * 
+ * @param  {MouseEvent/TouchEvent} evt tho on press event
+ */
+function onpress(evt) {
 
     switch (currentstate) {
 
+	// change state and update bird velocity
         case states.Splash:
             currentstate = states.Game;
             bird.jump();
             break;
 
+		// update bird velocity
         case states.Game:
             bird.jump();
             break;
@@ -189,6 +228,9 @@ function onpress() {
     }
 }
 
+/**
+ * Starts and initiate the game
+ */
 function main() {
     // create canvas and set width/height
     canvas = document.createElement("canvas");
@@ -205,20 +247,21 @@ function main() {
        evt = "mousedown";
     }
     // change state
+     // listen for input event
     document.addEventListener(evt, onpress);
 
-    canvas.width = width;
+	canvas.width = width;
 	canvas.height = height;
 	if (!(!!canvas.getContext && canvas.getContext("2d"))) {
 		alert("Your browser doesn't support HTML5, please update to latest version");
 	}
 	ctx = canvas.getContext("2d");
 
-    currentstate = states.Splash;
-        
-    // append canvas to document
+	currentstate = states.Splash;
+	// append canvas to document
 	document.body.appendChild(canvas);
 
+	// initate graphics and okbtn
     var img = new Image();
     img.onload = function() {
         initSprites(this);
@@ -226,54 +269,67 @@ function main() {
         run();
     }
     img.src = "res/sheet.png";
-
 }
 
+/**
+ * Starts and update gameloop
+ */
 function run() {
-    var loop = function() {
-        update();
-        render();
-        window.requestAnimationFrame(loop, canvas);
-    }
-    window.requestAnimationFrame(loop, canvas);
+	var loop = function() {
+		update();
+		render();
+		window.requestAnimationFrame(loop, canvas);
+	}
+	window.requestAnimationFrame(loop, canvas);
 }
 
-function update(){
-    frames++;
+/**
+ * Update forground, bird and pipes position
+ */
+function update() {
+	frames++;
 
-    if (currentstate !== states.Score) {
-        fgpos = (fgpos - 2) % 14;
-    } else {
-        best = Math.max(best, score);
-        localStorage.setItem("best", best);
-    }
-    if (currentstate === states.Game) {
-        pipes.update();
-    }
-    bird.update();
+	if (currentstate !== states.Score) {
+		fgpos = (fgpos - 2) % 14;
+	} else {
+		// set best score to maximum score
+		best = Math.max(best, score);
+		localStorage.setItem("best", best);
+	}
+	if (currentstate === states.Game) {
+		pipes.update();
+	}
+
+	bird.update();
 }
 
+/**
+ * Draws bird and all pipes and assets to the canvas
+ */
 function render() {
+    // draw background color
     ctx.fillRect(0, 0, width, height);  // fill in the background color
-
+    // draw background sprites
     s_bg.draw(ctx, 0, height - s_bg.height ); // initial picture
     s_bg.draw(ctx, s_bg.width, height - s_bg.height ); // fill in the gap
 
-    bird.draw(ctx);
     pipes.draw(ctx);
+    bird.draw(ctx);
 
+    // draw forground sprites
     s_fg.draw(ctx, fgpos, height - s_fg.height); // forground picture
     s_fg.draw(ctx, fgpos+s_fg.width, height - s_fg.height); // fill in the rest of the forground
 
     // show the splash screen and the "Get Ready" text
-    var width2 = width/2;
+    var width2 = width/2; // center of canvas
 
-    if (currentstate === states.Splash) {
-        // show splash screen
+	if (currentstate === states.Splash) {
+		// draw splash text and sprite to canvas
         s_splash.draw(ctx, width2 - s_splash.width/2, height - 300);
         // show the get ready text
         s_text.GetReady.draw(ctx, width2 - s_text.GetReady.width/2, height - 380);
     }
 }
 
+// start and run the game
 main();
