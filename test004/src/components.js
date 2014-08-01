4bdbfd9ab74be1e8f3e6a48b5815f0359fdd8bfb
PaulFSherwood <paulfsherwood@gmail.com>
2014-08-01 01:42:19 -0600

    add collision

--- test004/src/components.js
+++ test004/src/components.js
@@ -27,25 +27,42 @@ Crafty.c('Actor', {
   },
 });
 
-// This is the player-controlled character
-Crafty.c('PlayerCharacter', {
-    init: function() {
-        this.requires('Actor, Fourway, Color')
-            .fourway(4)
-            .color('rgb(20, 75, 40)');
-    }
-});
- 
 Crafty.c('Tree', {
     init: function() {
-        this.requires('2D, Canvas, Grid, Color');
+        this.requires('Actor, Color, Solid');
         this.color('rgb(20, 125, 40)');
     },
 });
 
 Crafty.c('Bush', {
     init: function() {
-        this.requires('2D, Canvas, Grid, Color');
+        this.requires('Actor, Color, Solid');
         this.color('rgb(20, 185, 40)');
     },
+});
+
+// This is the player-controlled character
+Crafty.c('PlayerCharacter', {
+    init: function() {
+        this.requires('Actor, Fourway, Color, Collision')
+            .fourway(4)
+            .color('rgb(20, 75, 40)')
+            .stopOnSolids();
+    },
+  // Registers a stop-movement function to be called when
+  //  this entity hits an entity with the "Solid" component
+  stopOnSolids: function() {
+    this.onHit('Solid', this.stopMovement);
+ 
+    return this;
+  },
+ 
+  // Stops the movement
+  stopMovement: function() {
+    this._speed = 0;
+    if (this._movement) {
+      this.x -= this._movement.x;
+      this.y -= this._movement.y;
+    }
+  }
 });
