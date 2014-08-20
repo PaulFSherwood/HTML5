9a02406747a73f969b4e575c13befbfaa111cc10
PaulFSherwood <paulfsherwood@gmail.com>
2014-08-19 20:56:32 -0600

    change to rock paper scissors

--- test006/test.js
+++ test006/test.js
@@ -1,40 +1,66 @@
-// create the function
-var checkWinner = function (userChoice) {
-    "use strict";
-    var isWinning, usrNum, cpuNum = Math.floor(Math.random() * 3) + 1;
-    if (userChoice === 'rock') {
-        usrNum = 1;
-    } else if (userChoice === 'paper') {
-        usrNum = 2;
-    } else {
-        // default value
-        usrNum = 3;
-    }
-    // check if you beat computer
-    // WIN
-    if ((usrNum === 1 && cpuNum === 3) || (usrNum === 2 && cpuNum === 1) || (usrNum === 3 && cpuNum === 2)) {
-        // rock beats scissors
-        isWinning = 1; // win
-    // LOOSE
-    } else if ((usrNum === 1 && cpuNum === 2) || (usrNum === 2 && cpuNum === 3) || (usrNum === 3 && cpuNum === 1)) {
-        // rock looses to paper
-        isWinning = 2; // fail
-    // DRAW
-    } else if ((usrNum === 1 && cpuNum === 1) || (usrNum === 2 && cpuNum === 2) || (usrNum === 3 && cpuNum === 3)) {
-        isWinning = 3; // tie
-    }
-    // Show what happend
-    if (isWinning === 1) {
-        console.log("You won");
-    } else if (isWinning === 2) {
-        console.log("You lose");
-    } else if (isWinning === 3) {
-        console.log("oh a tie, try again");
+var userChoice = prompt("Rock, Paper, Scissors, Lizard, Spock");
+
+var computerChoice = Math.floor(Math.random() * 5) + 1;
+if (computerChoice === 1) {
+	computerChoice = "Rock";
+} else if (computerChoice === 2) {
+	computerChoice = "Paper";
+} else if (computerChoice === 3) {
+    computerChoice = "Scissors";
+} else if (computerChoice === 3) {
+    computerChoice = "Lizard";
+} else if (computerChoice === 3) {
+    computerChoice = "Spock";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function ( choice1, choice2 )
+{
+    if ( choice1 === choice2 )
+    {
+        return "The result is a tie!";
+    } else if ( choice1 === "Rock" ) {
+        if ( choice2 === "scissors" ) {
+            return "Rock wins!";
+        } else if ( choice2 === "Lizard" ) {
+            return "Rock wins!";
+        }else {
+            return choice2 + " wins";
+        }
+    } else if ( choice1 === "Paper" ) {
+        if ( choice2 === "Rock" ) {
+            return "Paper wins";
+        } else if ( choice2 === "Spock" ) {
+            return "Paper wins";
+        } else {
+            return choice2 + " wins";
+        }
+    } else if ( choice1 === "Scissors" ) {
+        if ( choice2 === "Lizard" ) {
+            return "Scissors wins";
+        } else if ( choice2 === "Paper" ) {
+            return "Scissors wins";
+        } else {
+            return choice2 + " wins";
+        }
+    }  else if ( choice1 === "Lizard" ) {
+        if ( choice2 === "Spock" ) {
+            return "Lizard wins";
+        } else if ( choice2 === "Paper" ) {
+            return "Lizard wins";
+        } else {
+            return choice2 + " wins";
+        }
+    }  else if ( choice1 === "Spock" ) {
+        if ( choice2 === "Rock" ) {
+            return "Spock wins";
+        } else if ( choice2 === "Scissors" ) {
+            return "Spock wins";
+        } else {
+            return choice2 + " wins";
+        }
     }
 }
 
-// get user choice
-var userChoice = prompt("Pleace choose 'rock' 'paper'or 'scissors'");
-
-// call check function to find out who won
-checkWinner(userChoice);
+compare( userChoice, computerChoice );
