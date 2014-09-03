8644b36ee29698c0ce7bf379969b48fe93b45704
PaulFSherwood <paulfsherwood@gmail.com>
2014-09-02 20:33:12 -0600

    still trying text under mouse thing

--- test007/img_map.js
+++ test007/img_map.js
@@ -1,17 +1,8 @@
-    $(document).ready(function() {
-        if($('#map')) {
-            $('#map area').each(function() {
-                var id = $(this).attr('id');
-                $(this).mouseover(function() {
-                    $('#overlay'+id).show();
-                    
-                });
-                
-                $(this).mouseout(function() {
-                    var id = $(this).attr('id');
-                    $('#overlay'+id).hide();
-                });
-            
-            });
-        }
-    });
+function writeText(txt)
+{
+    console.log("called!");
+    //document.getElementById("desc").innerHTML=txt;
+
+
+
+}
