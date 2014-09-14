75c7ba0225adc11b830c17c035fc3b7c7b9977d7
PaulFSherwood <paulfsherwood@gmail.com>
2014-09-14 08:13:24 -0600

    object testing

---
+++ test008/fiat.js
@@ -0,0 +1,78 @@
+var vehicle = {
+    make: "",
+    model: "500",
+    year: 1900,
+    color: "",
+    passengers: 2,
+    convertable: false,
+    mileage: 1,
+    started: false
+}
+
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000,
+    started: false,
+
+    start: function() {
+        this.started = true;
+    },
+
+    stop: function() {
+        this.started = false;
+    },
+
+    drive: function() {
+        if (this.started) {
+            alert("Zoom zoom!");
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+};
+
+var cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892,
+    started: false,
+};
+
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021,
+    started: false,
+};  
+
+var taxi = {
+    make: "Webville Motors",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341,
+    started: false,
+};
+
+for (var prop in chevy) {
+    console.log(prop + ": " + chevy[prop]);
+}
+
+for (var prop in fiat) {
+    console.log(prop + ": " + fiat[prop]);
+}
