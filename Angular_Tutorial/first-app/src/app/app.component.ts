f69ec51c816b8e13d3cdb2702a0bd6334c451862
PaulFSherwood <paulfsherwood@gmail.com>
2020-11-19 15:23:21 -0700

    Finishing the Built-in Directives, not what I expected.  Seems usefull

--- Angular_Tutorial/first-app/src/app/app.component.ts
+++ Angular_Tutorial/first-app/src/app/app.component.ts
@@ -6,13 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'bacon';                       // Variables for the html f
-  ServerName = 'Apollo';
-  ServerPID = 11;
-  ServerStatus = 'offline';
-  statusFlag = false;
-  buttonState = true;
-  FName = '';
+  name              = 'bacon';                       // Variables for the html f
+  ServerName        = 'Apollo';
+  ServerPID         = 11;
+  ServerStatus      = 'offline';
+  statusFlag        = false;
+  buttonState       = true;
+  FName             = '';
+  buttonStateFName  = true;
+  flag = true;
+  paraFlagCounter = 1;
+  paraFlag = true;
+  paraSetStyle = false;
+
+  studentRoster = ['bob', 'tim', 'jen'];
+  currentName = '';
 
   constructor() {
     setTimeout(() => {
@@ -32,6 +40,41 @@ export class AppComponent {
   }
   clearFNameField(){
     this.FName = '';
+    // this.buttonStateFName = false;
+  }
+
+  toggleFlag() {
+    this.flag = !this.flag;
+    return this.flag;
+  }
+
+  getColor() {
+    if (this.flag === true ){
+      return 'green';
+    } else {
+      return 'red';
+    }
+  }
+
+  add() {
+    if (this.currentName != '') {
+      this.studentRoster.push(this.currentName);
+    }
+  }
+
+  toggleParagraph() {
+    this.paraFlag = !this.paraFlag;
+
+    console.log(this.paraFlagCounter);
+
+    if (this.paraFlagCounter < 5) {
+      this.paraFlagCounter++;
+      return this.paraFlag;
+    } else if (this.paraFlagCounter >= 5 ) {
+      return this.paraSetStyle = true;
+    } else {
+      return this.paraSetStyle;
+    }
   }
 
 }
