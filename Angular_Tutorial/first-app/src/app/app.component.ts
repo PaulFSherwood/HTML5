1839f116a760506163beb3e4602f365962b9e6db
PaulFSherwood <paulfsherwood@gmail.com>
2020-11-19 12:48:02 -0700

    Fixed the server not compiling.  Shows everhing correctly

--- Angular_Tutorial/first-app/src/app/app.component.ts
+++ Angular_Tutorial/first-app/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'bacon';                       // Variables for the html f
+  ServerName = 'Apollo';
+  ServerPID = 11;
+  ServerStatus = 'offline';
+  statusFlag = false;
+  buttonState = true;
+
+  constructor() {
+    setTimeout(() => {
+      this.buttonState = false;
+    }, 2500);
+  }
+  
+  toggleServerStatus() {
+
+    this.statusFlag = !this.statusFlag;
+    if (this.statusFlag === true) {
+      this.ServerStatus = 'online';
+    } else {
+      this.ServerStatus = 'offline';
+    }
+    return this.ServerStatus;
+  }
+
 }
