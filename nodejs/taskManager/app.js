a5cbe6d0fea15d63d084ba33a2e61bff3d095ff8
PaulFSherwood <paulfsherwood@gmail.com>
2020-11-20 21:13:48 -0700

    starting nodejs, just finished how to read and write to a file

---
+++ nodejs/taskManager/app.js
@@ -0,0 +1,43 @@
+// var firstName = 'EduBacon';
+
+// console.log(firstName);
+// firstName = 10;
+// console.log(firstName);
+// var age1 = 30;
+// var age2 = '30';
+// var result = age1 == age2;
+// console.log(result);
+// var result = age1 === age2;
+// // console.log(result);
+// function sayHello(name) {
+//     return ('Hello ' + name + ' !!!');
+// }
+// console.log(sayHello('Paul'));
+
+// // anonymous function
+// var sayHello = function(name) {
+//     return ('Hello ' + name + ' !!!');
+// }
+// console.log(sayHello('Paul'));
+
+// // Objects
+// var student = {
+//     name: 'Fname',
+//     email: 'Fname@gmail.com'
+// }
+// console.log(student.name);
+
+// File System
+// var fs = require('fs');
+// fs.writeFile('./hello.txt', 'How are you?', function(err) {
+//     if (!err) {
+//         fs.readFile('./hello.txt', function(err, data) {
+//             if (!err) {
+//                 console.log(data.toString());
+//             }
+//         });
+//     }
+// })
+
+// Events
+
