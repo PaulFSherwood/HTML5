da31b2d320af2b82015d7b73652d671ac5feb722
PaulFSherwood <paulfsherwood@gmail.com>
2020-11-20 22:23:09 -0700

    Finished a basic server with pulling information from a json file and displaing it

--- nodejs/taskManager/app.js
+++ nodejs/taskManager/app.js
@@ -42,22 +42,50 @@
 // Events
 
 // CREATE A SERVER
+// var http = require('http');
+// var events = require('events');
+
+// var eventEmitter = new events.EventEmitter();
+
+// var server = http.createServer(function(req, res) {
+//     eventEmitter.emit('someone requested', 'TEST'); // Event Trigger
+//     res.end('Server Works!!!');
+// });
+
+// eventEmitter.on('someone requested', function(data) {
+//     console.log('A request has been done on the server!', data);
+// }); // Event Listener
+
+// server.listen(3000, 'localhost', function() {
+//     console.log('Server started on port : 3000');
+// });
+
+
+var express = require('express');
 var http = require('http');
-var events = require('events');
+var fs = require('fs');
 
-var eventEmitter = new events.EventEmitter();
+var app = express();
+var server = http.createServer(app);
 
-var server = http.createServer(function(req, res) {
-    eventEmitter.emit('someone requested', 'TEST'); // Event Trigger
-    res.end('Server Works!!!');
+// address the get request
+app.get('/', function(req, res) {
+    // Show working and test html
+    res.send('<h1>Express Works!</h1>');
 });
 
-eventEmitter.on('someone requested', function(data) {
-    console.log('A request has been done on the server!', data);
-}); // Event Listener
-
-server.listen(3000, 'localhost', function() {
-    console.log('Server started on port : 3000');
+// Looks like this adds another directory tree
+app.get('/tasks', function(req, res) {
+    // res.send('<h1>Tasks work</h1>');
+    fs.readFile('./db.json', function(err, data) {
+        // Convert to json toString wont have tasks
+        var tasks = JSON.parse(data.toString()).tasks;
+        // res.send(data.toString());
+        res.send(tasks);
+    })
 });
-
-
+// Start server on port 3000
+// app.listen(3000, function() {
+server.listen(3000, function() {
+    console.log('Server listing to port 3000');
+});
