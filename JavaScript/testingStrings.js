720f00619d2d942c4452724f7d0a44ffc45c2c68
PaulFSherwood <paulfsherwood@gmail.com>
2020-12-01 06:23:32 -0700

    Trying to workout hiding rows

---
+++ JavaScript/testingStrings.js
@@ -0,0 +1,5 @@
+if (document.getElementsByTagName("tr")[18].getElementsByTagName("td")[2].innerText == "the") { console.log("true");}
+
+
+
+<tr><td class="strongsnt" valign="top"><span class="strongsnt"><a href="https://biblehub.com/greek/3588.htm" title="Strong&#39;s Greek 3588: The, the definite article. Including the feminine he, and the neuter to in all their inflections; the definite article; the.">3588</a></span> <span class="strongsnt"><a href="https://biblehub.com/greek/strongs_3588.htm" title="Englishman&#39;s Greek: 3588">[e]</a></span></td><td class="greek2" valign="top">τὸ<span class="translit"><a href="https://biblehub.com/greek/to_3588.htm" title="to: the.">to</a></span></td><td class="eng" valign="top">the</td><td class="pos" valign="top"><span class="pos"><a href="https://biblehub.com/grammar/greek.htm" title="Article - Accusative Neuter Singular">Art-ANS</a></span></td></tr>
