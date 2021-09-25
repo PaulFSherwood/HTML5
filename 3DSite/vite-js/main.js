8a5f55fec5f1e2d8439b010b45a9c9982f0ade3e
PaulFSherwood <paulfsherwood@gmail.com>
2021-09-24 19:28:55 -0600

    Camera and controls are working
    I went back and tried adding controls to the other page, but I don't really undstand what I am doing enough to do this.  Seems exiting though.  Adding these models is crazy cool.

--- 3DSite/vite-js/main.js
+++ 3DSite/vite-js/main.js
@@ -1,6 +1,9 @@
 import './style.css'
 import * as THREE from 'three';
-import { TorusGeometry } from 'three';
+import { PointLight, TorusGeometry } from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 
@@ -16,11 +19,46 @@ camera.position.setZ(30);
 
 renderer.render( scene, camera );
 
+// Create a model and asign material to that model
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true });
+// const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true });
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347 });
 const torus = new THREE. Mesh( geometry, material);
 
-scene.add(torus)
+// add model to the scene
+// scene.add(torus);  // this is annoying when you don't need it.
+
+const pointLight = new THREE.PointLight( 0xFFFFFF );
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight( 0xFFFFFF );
+
+// add the all ligths to the scene
+scene.add(pointLight, ambientLight);
+
+// show where the light is at.
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper( 200, 50 );
+scene.add(lightHelper, gridHelper);
+
+// add mouse controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+// let loader = new THREE.GLTFLoader();
+
+// loader.load('SpaceShip.glb', function(gltf){
+//   scene.add(gltf.scene);
+//   renderer.render(scene, camera);
+// });
+
+const loader = new GLTFLoader();
+
+loader.load('SpaceShip.glb', function(gltf) {
+  scene.add(gltf.scene);
+}, undefined, function (error) {
+  console.error(error);
+});
+
 
 function animate() {
   requestAnimationFrame( animate );
@@ -28,7 +66,9 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
-  
+
+  controls.update();
+
   renderer.render( scene, camera );
 }
 
