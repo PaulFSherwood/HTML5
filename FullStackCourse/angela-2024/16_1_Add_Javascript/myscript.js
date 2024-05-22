78f557861210aeb8b6a22a734eef013fe0b680d9
PaulFSherwood <PaulFSherwood@gmail.com>
2024-05-21 18:36:53 -0600

    Added some DOM testing, completed a challenge.

--- FullStackCourse/angela-2024/16_1_Add_Javascript/myscript.js
+++ FullStackCourse/angela-2024/16_1_Add_Javascript/myscript.js
@@ -1,9 +1,9 @@
 
 // Window.onload
-window.onload=function() {
-    console.log('Hello {window.onload}');
-    document.querySelector('H1').innerHTML = "done-ish";
-}
+// window.onload=function() {
+//     console.log('Hello {window.onload}');
+//     document.querySelector('H1').innerHTML = "done-ish";
+// }
 
 // OR
 // regular calling line
@@ -15,3 +15,34 @@ window.onload=function() {
 //     // some code
 //     console.log("DOM loaded {document.addEventListener}");
 // });
+// window.onload = function() {
+//     setTimeout(function() {
+//         // Your code here
+//         document.querySelector('H1').innerHTML = "3 Seconds done.";
+//     }, 3000); // 3000 milliseconds = 3 seconds
+// };
+
+
+window.addEventListener("load", function(event) {
+    // First function code
+    console.log('Hello {window.onload}');
+
+
+    setTimeout(function() {
+        document.querySelector('H1').innerHTML = "done-ish";
+    }, 1500);
+
+    // After 3 seconds, change the H1 text
+    setTimeout(function() {
+        document.querySelector('H1').innerHTML = "3 Seconds done.";
+    }, 3000); // 3000 milliseconds = 3 seconds
+
+    this.setTimeout(function() {
+        // var items = document.querySelectorAll('.list');
+        // var thirdItem = items[2];
+
+        // thirdItem.textContent = "Late to the party";
+        document.querySelector('ul > li:nth-child(3)').textContent = "Late to the party.";
+    }, 4500);
+
+}, false);
