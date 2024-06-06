5a6f50851daef2d1dc795c3624cec83a26f169f7
PaulFSherwood <PaulFSherwood@gmail.com>
2024-06-06 17:03:53 -0600

    Completed the Simon game

--- FullStackCourse/angela-2024/20_2_Simon_Game/index.js
+++ FullStackCourse/angela-2024/20_2_Simon_Game/index.js
@@ -1,20 +1,30 @@
 let newGame = true;
 let colorChoices = ["red", "green", "blue", "yellow"];
 let colorArray = [];
-let colorCount = colorArray.length;
+let colorCount = 0;//colorArray.length;
 let isRunning = true;
 
 
-$(".block").on("click", function() {
-    // console.log($(this).prop("classList")[0]);
-    colorArray.push($(this).prop("classList")[0]);
-    // console.log(colorArray);
-    // getColor();
-});
+// $(".block").on("click", function() {
+//     // console.log($(this).prop("classList")[0]);
+//     colorArray.push($(this).prop("classList")[0]);
+//     // console.log(colorArray);
+//     // getColor();
+// });
 
 function getColor() {
     return colorChoices[Math.floor(Math.random() * (0, 3))];
     // console.log("(" + color + ")");
+}
+
+function addColorResetArray() {
+    let color = getColor();  // Make sure to declare 'color' with 'let' or 'const'
+    console.log("Color added: " + color);
+
+    colorArray.push(color);
+    // update the color count
+    colorCount = 0;//colorArray.length;
+    console.log("Current color Array: " + colorArray);
 }
 
 $(document).ready(function() {
@@ -29,32 +39,76 @@ $(document).ready(function() {
     });
 });
 
-function pulseCube(color) {
-    $(".bottom-text").text("");
-    $("." + color).addClass("pulse");
+// function pulseCube(color) {
+//     // $(".bottom-text").text("");
+//     $("." + color).removeClass("pulse").css("animation", "none")[0].offsetHeight;
 
-    setTimeout(function() {
-        $("." + color).removeClass("pulse");
-    }, 1000);  // Remove 'pulse' class after 1 second
-};
+//     setTimeout(function() {
+//         $("." + color).addClass("pulse").css("animation", "");
+//     }, 1300);  // Remove 'pulse' class after 1 second
+// };
+function pulseCube(color) {
+    var element = $("." + color);
+
+    // Increment a counter to change the animation name, making it unique each time
+    var animationName = 'pulse' + Date.now();  // Unique key using timestamp
+
+    // Define a unique keyframes animation using style elements
+    var keyframes = `@keyframes ${animationName} {
+        0%, 100% { transform: scale(1); opacity: 1; }
+        50% { transform: scale(1.2); opacity: 0.7; }
+    }`;
+
+    // Append the keyframes to the document head or update existing style
+    var styleTag = $('#pulseKeyframesStyle');
+    if (styleTag.length === 0) {
+        $('head').append(`<style id='pulseKeyframesStyle'>${keyframes}</style>`);
+    } else {
+        styleTag.html(keyframes);  // Replace existing keyframes to ensure the new one is used
+    }
+
+    // Apply the new animation
+    element.css({
+        animationName: animationName,
+        animationDuration: '1s',
+        animationTimingFunction: 'ease-in-out',
+        animationFillMode: 'forwards'
+    });
+
+    // Optional: Clear the animation after it's complete to clean up
+    setTimeout(() => {
+        element.css('animationName', 'none');
+    }, 1000);  // Match the duration of the animation
+}
+
+
+
+function pulseAllCubes() {
+    let delay = 1300;  // Base delay of 1 second
+
+    for (let i = colorArray.length-1; i >= 0; i--) {
+        console.log("Color pulsed: " + colorArray[i]);
+        setTimeout(function() {
+            pulseCube(colorArray[i]);
+        }, delay * i);  // Delay increases with each iteration
+    }
+
+}
 
 function runGame() {
-
+    console.log("runGame: ");
     $(".bottom-text").text("Please Wait.");
     // Get initial color then flash the cube for a second
-    let color = getColor();  // Make sure to declare 'color' with 'let' or 'const'
-    console.log("Color added: " + color);
+    addColorResetArray();
 
-    colorArray.push(color);
-    // update the color count
-    colorCount = colorArray.length;
-    console.log("Current color Array: " + colorArray);
+    // // Pulse all colors
+    // for (let clrs of colorArray) {
+    //     console.log("Color pulsed: " + clrs);
+    //     pulseCube(clrs);
+    // }
 
-    // Pulse all colors
-    for (let clrs of colorArray) {
-        console.log("Color pulsed: " + clrs);
-        pulseCube(clrs);
-    }
+    pulseAllCubes();
+
 
     console.log("Current Color Sequence:", colorArray);
     console.log("Color count: " + colorCount);
@@ -67,14 +121,19 @@ $(document).on("click", ".block", function() {
     // console.log("clicked: " + colorArray[colorCount - 1]);
     // console.log("clicked: " + $(this).attr('class'));
     // We should just be able to check clicks against the current color and pass or fail.
-    if (colorCount >= 1) {
+    if (colorCount <= colorArray.length) {
         // Game is still on
-        if ($(this).hasClass(colorArray[colorCount - 1])) {
+        if ($(this).hasClass(colorArray[colorCount])) {
             console.log("match");
             // subtract from colorCount
-            colorCount -= 1;
-            if (colorCount === 0 && isRunning === true) {
-                $(".bottom-text").text("You Win!");
+            colorCount += 1;
+            if (colorCount === colorArray.length && isRunning === true) {
+                console.log("Color count is 0");
+                // Add a new color to the array
+                addColorResetArray();
+                
+                // Pulse all cubes
+                pulseAllCubes();
             }
         } else {
             $(".bottom-text").text("Game Failed!");
@@ -84,6 +143,8 @@ $(document).on("click", ".block", function() {
         // if ($(this).attr('class') === colorArray[colorCount - 1]) {
         //     console.log("match2");
         // }
+    } else {
+
     }
 });
 
