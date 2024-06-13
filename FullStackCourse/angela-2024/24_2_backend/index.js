b4972fd1db702900eb0e637fef4a2b3cca08e5e6
PaulFSherwood <PaulFSherwood@gmail.com>
2024-06-13 16:53:23 -0600

    test update

--- FullStackCourse/angela-2024/24_2_backend/index.js
+++ FullStackCourse/angela-2024/24_2_backend/index.js
@@ -1,7 +1,16 @@
-import express from "express";
-const app = express();
-const port = 3000;
+// import express from "express";
+// const app = express();
+// const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}.`);
+// });
+
+
+// var http = require('http');
+import http from "http";
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
