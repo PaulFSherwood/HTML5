d396e984cf2bec4b3a02bd902a0038baf1140f67
PaulFSherwood <PaulFSherwood@gmail.com>
2024-06-13 18:31:46 -0600

    working with nodemon and serving webpages.

--- FullStackCourse/angela-2024/24_2_backend/index.js
+++ FullStackCourse/angela-2024/24_2_backend/index.js
@@ -1,16 +1,37 @@
-// import express from "express";
-// const app = express();
-// const port = 3000;
+import express from "express";
+const app = express();
+const port = 3000;
 
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}.`);
-// });
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
 
+app.get("/about", (req, res) => {
+    res.send(`
+        <head>
+            <title>About Me</title>
+        </head>
+        <body>
+            <h1>About Me</h1>
+            <p>My name is [Your Name].</p>
+            <p>I live in [Your City].</p>
+            <p>I enjoy [Your Hobby].</p>
+        </body>
+    `);
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact me</h1><h2>paul</h2>");
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>");
+});
 
 // var http = require('http');
-import http from "http";
+// import http from "http";
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World!');
+// }).listen(8080);
