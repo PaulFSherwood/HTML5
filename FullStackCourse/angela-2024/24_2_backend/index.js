a0fa4f319324bb4a9b670b72f98a3a97ae5db003
PaulFSherwood <PaulFSherwood@gmail.com>
2024-06-17 17:39:22 -0600

    more work with nodejs and middleware

--- FullStackCourse/angela-2024/24_2_backend/index.js
+++ FullStackCourse/angela-2024/24_2_backend/index.js
@@ -27,6 +27,23 @@ app.get("/contact", (req, res) => {
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+app.post("/register", (req, res) => {
+    res.sendStatus(201);
+});
+
+app.put("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.patch("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.delete("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
 
 // var http = require('http');
 // import http from "http";
