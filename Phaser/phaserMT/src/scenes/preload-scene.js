c247279b2e957645ae6813e1e8a0be73efa01bc7
PaulFSherwood <PaulFSherwood@gmail.com>
2024-10-24 17:22:46 -0600

    finished vid 6 (Monster Tamer - RPG Tutorial with Phaser 3 ep 5)

--- Phaser/phaserMT/src/scenes/preload-scene.js
+++ Phaser/phaserMT/src/scenes/preload-scene.js
@@ -1,3 +1,4 @@
+import { BATTLE_ASSET_KEYS, BATTLE_BACKGROUND_ASSET_KEYS, HEALTH_BAR_ASSET_KEYS, MONSTER_ASSET_KEYS } from "../assets/asset-keys";
 import Phaser from "../lib/phaser";
 import { SCENE_KEYS } from "./scene-keys";
 
@@ -15,13 +16,53 @@ export class PreloadScene extends Phaser.Scene {
     // }
 
     preload() {
+        const monsterTamerAssetPath = 'assets/images/monster-tamer'
+        const kenneysAssetPath = 'assets/images/kenneys-assets'
+
+        
+        // Battle Backgrounds
         this.load.image(
-            'background', 
-            'assets/images/monster-tamer-bg.png');
+            BATTLE_BACKGROUND_ASSET_KEYS.FOREST, 
+            `${monsterTamerAssetPath}/battle-backgrounds/forest-background.png`
+        );
+
+        // Battle Assets
+        this.load.image(
+            BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND, 
+            `${kenneysAssetPath}/ui-space-expansion/custom-ui.png`
+        );
+
+        // Health Bar
+        this.load.image(
+            HEALTH_BAR_ASSET_KEYS.RIGHT_CAP, 
+            `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_right.png`
+        );
+        this.load.image(
+            HEALTH_BAR_ASSET_KEYS.MIDDLE, 
+            `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_mid.png`
+        );
+        this.load.image(
+            HEALTH_BAR_ASSET_KEYS.LEFT_CAP, 
+            `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_left.png`
+        );
+
+        // Monster Assets
+        this.load.image(
+            MONSTER_ASSET_KEYS.CARNODUSK, 
+            `${monsterTamerAssetPath}/monsters/carnodusk.png`
+        );
+        this.load.image(
+            MONSTER_ASSET_KEYS.IGUANIGNITE, 
+            `${monsterTamerAssetPath}/monsters/iguanignite.png`
+        );
     }
 
     create() {
-        this.textures.get('background');//.setFilter(Phaser.Textures.FilterMode.NEAREST);
+        console.log('create');
+        console.log(this.textures.get('background'));//.setFilter(Phaser.Textures.FilterMode.NEAREST);
+
+        // test screen output
+        this.add.image(0, 0, BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
     }
 
     // update() {
