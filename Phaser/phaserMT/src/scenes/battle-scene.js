50c82bc08db8edd06614959ec1fa9c24734de072
PaulFSherwood <PaulFSherwood@gmail.com>
2024-10-29 06:21:50 -0600

    almost done with adding menus and text 10/128

--- Phaser/phaserMT/src/scenes/battle-scene.js
+++ Phaser/phaserMT/src/scenes/battle-scene.js
@@ -6,6 +6,18 @@ import {
 import Phaser from "../lib/phaser";
 import { SCENE_KEYS } from "./scene-keys";
 
+const BATTLE_MENU_OPTIONS = Object.freeze( {
+    FIGHT: 'FIGHT',
+    SWITCH: 'SWITCH',
+    ITEM: 'ITEM',
+    FLEE: 'FLEE',
+});
+
+const battleUiTextStyle = {
+    color: 'black',
+    fontSize: '30px',
+}
+
 export class BattleScene extends Phaser.Scene {
     constructor() {
         super({
@@ -37,11 +49,11 @@ export class BattleScene extends Phaser.Scene {
                 .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
                 .setOrigin(0),
             playerMonsterName,
-            this.#createHealth(34, 34),
-            this.add.text( playerMonsterName.width + 35, 23, 'L5', {
-                    color: '#ED474B',
-                    fontSize: '28px',
-                }),
+            this.#createHealthBar(34, 34),
+            this.add.text(playerMonsterName.width + 35, 23, 'L5', {
+                color: '#ED474B',
+                fontSize: '28px',
+            }),
             this.add.text(30, 55, 'HP', {
                 color: '#FF6505',
                 fontSize: '24px',
@@ -68,33 +80,56 @@ export class BattleScene extends Phaser.Scene {
                 .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
                 .setOrigin(0).setScale(1, 0.8),
             enemyMonsterName,
-            this.#createHealth(34, 34),
+            this.#createHealthBar(34, 34),
             this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
-                    color: '#ED474B',
-                    fontSize: '28px',
-                }),
+                color: '#ED474B',
+                fontSize: '28px',
+            }),
             this.add.text(30, 55, 'HP', {
                 color: '#FF6505',
                 fontSize: '24px',
                 fontStyle: 'italic',
             }),
         ]);
+
+        // Rende out the main info and sub info panes
+        this.#createMainInfoPane();
+        this.add.container(520, 448, [
+            this.#createMainInfoSubPane(),
+            this.add.text(55, 22, BATTLE_MENU_OPTIONS.FIGHT, battleUiTextStyle),
+            this.add.text(240, 22, BATTLE_MENU_OPTIONS.SWITCH, battleUiTextStyle),
+            this.add.text(55, 70, BATTLE_MENU_OPTIONS.ITEM, battleUiTextStyle),
+            this.add.text(240, 70, BATTLE_MENU_OPTIONS.FLEE, battleUiTextStyle),
+        ]);
     }
-    #createHealth(x, y) {
+    #createHealthBar(x, y) {
         const scaleY = 0.7;
         const leftCap = this.add
-        .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
-        .setOrigin(0, 0.5)
-        .setScale(1, scaleY);
+            .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
+            .setOrigin(0, 0.5)
+            .setScale(1, scaleY);
         const middle = this.add
-        .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
-        .setOrigin(0, 0.5)
-        .setScale(1, scaleY);
+            .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+            .setOrigin(0, 0.5)
+            .setScale(1, scaleY);
         middle.displayWidth = 360;
         const rightCap = this.add
-        .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
-        .setOrigin(0, 0.5)
-        .setScale(1, scaleY);
+            .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
+            .setOrigin(0, 0.5)
+            .setScale(1, scaleY);
+
         return this.add.container(x, y, [leftCap, middle, rightCap]);
+    }
+
+    #createMainInfoPane() {
+        const padding = 4;
+        const rectHeight = 124;
+        this.add.rectangle(padding, this.scale.height - rectHeight - padding, this.scale.width - padding * 2, rectHeight, 0xE4080A, 0.2).setOrigin(0).setStrokeStyle(8, 0xe4434a, 1);
+    }
+
+    #createMainInfoSubPane() {
+        const rectWidth = 500;
+        const rectHeight = 124;
+        return this.add.rectangle(0, 0, rectWidth, rectHeight, 0xede4f3, 1).setOrigin(0).setStrokeStyle(8, 0x905ac2, 1);
     }
 }
